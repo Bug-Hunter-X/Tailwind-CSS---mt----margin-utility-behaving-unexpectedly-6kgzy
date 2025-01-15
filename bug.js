38c89b6c534a6bde1467f957d457501cffcf8d70
BugHunter-X <askzenai@gmail.com>
2025-01-15 16:58:06 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,15 @@
+```javascript
+// MyComponent.js
+function MyComponent() {
+  return (
+    <div className="bg-gray-100 p-4">
+      <h1 className="text-2xl font-bold">My Component</h1>
+      <div className="mt-4">
+        {/* Some content here */}
+      </div>
+    </div>
+  );
+}
+
+export default MyComponent;
+```
